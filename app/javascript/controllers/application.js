3d4de456be301df9383858fdcf4452739a9d864b
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-07-04 20:05:19 -0600

    first portfolio commit

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,14 @@
-import { Application } from "@hotwired/stimulus"
+import { Turbo, cable } from "@hotwired/turbo-rails";
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
-const application = Application.start()
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
+window.Turbo = Turbo;
 
-export { application }
+export { application };
